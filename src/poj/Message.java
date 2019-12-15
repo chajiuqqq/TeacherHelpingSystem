5e37a86982d1b706a5a15acd2ab2c1a75e7bd12f
chajiuqqq <chajiuqqq@outlook.com>
2019-12-15 18:39:29 +0800

    complete poj.Message

--- src/poj/Message.java
+++ src/poj/Message.java
@@ -1,5 +1,6 @@
 package poj;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
 
 @Entity
 @Table(name="message")
@@ -22,23 +27,46 @@ public class Message {
 	@JoinColumn(name="tid")
 	Teacher teacher;
 	
-	
-	int ts_id;
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="ts_id")
+	Teacher_Subject ts;
 	
 	
 	public int getId() {
 		return id;
 	}
-	public int getTs_id() {
-		return ts_id;
+	public Teacher getTeacher() {
+		return teacher;
 	}
-	
+	public Teacher_Subject getTs() {
+		return ts;
+	}
 	
 	public void setId(int id) {
 		this.id = id;
 	}
-	public void setTs_id(int ts_id) {
-		this.ts_id = ts_id;
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+	public void setTs(Teacher_Subject ts) {
+		this.ts = ts;
+	}
+	
+	public static void main(String[] args) {
+		SessionFactory sf=new Configuration().configure().buildSessionFactory();
+		Session s=sf.openSession();
+		s.beginTransaction();
+		Teacher t=(Teacher)s.get(Teacher.class,10001);
+		Teacher_Subject ts=(Teacher_Subject)s.get(Teacher_Subject.class, 13);
+		
+		Message message=new Message();
+		message.setTeacher(t);
+		message.setTs(ts);
+		s.save(message);
+		
+		s.getTransaction().commit();
+		s.close();
+		sf.close();
 	}
 	
 }
